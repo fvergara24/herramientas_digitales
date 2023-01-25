0462253caf08e35e9c93aa46cdbfcbf36f5e7cfb
fvergara24 <fvergara@gmail.com>
2023-01-25 19:30:02 +0000

    agregar app

--- app_git_colab.py
+++ app_git_colab.py
@@ -7,7 +7,7 @@ st.title('Empoweringrace')
 st.title('Herramientas digitales en español')
 st.title('dataframe')
 
-pip install BeautifulSoup
+!pip install BeautifulSoup
 
 import pandas as pd
 from bs4 import BeautifulSoup
