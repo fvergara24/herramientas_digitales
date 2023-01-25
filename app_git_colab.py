f06b3d2125ec7a99fa20cb4f51b973ccdca8a6f0
fvergara24 <fvergara@gmail.com>
2023-01-25 17:57:14 +0000

    agregar app

---
+++ app_git_colab.py
@@ -0,0 +1,8 @@
+
+import streamlit as st
+
+st.set_page_config(page_title='Herramientas digitales en español')
+st.title('Empoweringrace')
+
+st.title('Herramientas digitales en español')
+st.title('Hola a Majo')
