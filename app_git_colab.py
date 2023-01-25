b609b7337c23b5b1e3cf978bd59c10e1c3c96d91
fvergara24 <fvergara@gmail.com>
2023-01-25 19:36:51 +0000

    agregar app

--- app_git_colab.py
+++ app_git_colab.py
@@ -8,7 +8,7 @@ st.title('Herramientas digitales en español')
 st.title('dataframe')
 
 import pandas as pd
-from bs4 import BeautifulSoup
+import BeautifulSoup
 import requests
 from textblob import TextBlob
 
