7c7d9350fa6785b14b3b41a18837163f907ee45e
fvergara24 <fvergara@gmail.com>
2023-01-26 02:42:06 +0000

    agregar app

--- app_git_colab.py
+++ app_git_colab.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title='Herramientas digitales en español')
 st.title('Empoweringrace')
 
 st.title('Herramientas digitales en español')
-df=pd.read_csv('/content/herramientas_digitales/datos.csv')
+df=pd.read_csv('/herramientas_digitales/datos.csv')
 df=df.drop(columns='Unnamed: 0',axis=1)
 st.dataframe(data=df)
 st.title('dataframe')
