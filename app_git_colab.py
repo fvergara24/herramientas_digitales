d13afe5ef02ff2d2510353f36faa8e788708d660
fvergara24 <fvergara@gmail.com>
2023-01-26 02:41:14 +0000

    agregar app

--- app_git_colab.py
+++ app_git_colab.py
@@ -1,8 +1,12 @@
 
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(page_title='Herramientas digitales en español')
 st.title('Empoweringrace')
 
 st.title('Herramientas digitales en español')
+df=pd.read_csv('/content/herramientas_digitales/datos.csv')
+df=df.drop(columns='Unnamed: 0',axis=1)
+st.dataframe(data=df)
 st.title('dataframe')
