91f23d72f890e1de5078556b799167eb446497bf
fvergara24 <fvergara@gmail.com>
2023-02-02 17:44:24 +0000

    agregar app

--- app_git_colab.py
+++ app_git_colab.py
@@ -9,19 +9,19 @@ st.title('Herramientas digitales en español')
 df=pd.read_csv('datos.csv')
 df=df.drop(columns='Unnamed: 0',axis=1)
 
-imagen1=df['Imagenes_url'][0]+'.jpg'
+#imagen1=df['Imagenes_url'][0]+'.jpg'
 
 col1, col2, col3 = st.columns(3)
 
 with col1:
    st.header(df.iloc[0][0])
-   st.image(df.iloc[0][3], caption=df.iloc[0][3], use_column_width='auto')
+   #st.image(df.iloc[0][3], caption=df.iloc[0][3], use_column_width='auto')
    
 with col2:
    st.header(df.iloc[1][0])
-   st.image(df.iloc[1][3], caption=df.iloc[1][2])
+   #st.image(df.iloc[1][3], caption=df.iloc[1][2])
 
 with col3:
    st.header(df.iloc[2][0])
-   st.image(df.iloc[2][3], caption=df.iloc[2][2])
+   #st.image(df.iloc[2][3], caption=df.iloc[2][2])
 
