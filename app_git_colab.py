d3d460647fca1abf1d099bcf6a694a6886025ebe
fvergara24 <fvergara@gmail.com>
2023-02-07 23:25:26 +0000

    agregar app

--- app_git_colab.py
+++ app_git_colab.py
@@ -1,7 +1,6 @@
 
 import streamlit as st
 import pandas as pd
-import Div
 
 st.set_page_config(page_title='Herramientas digitales en español')
 st.title('Empoweringrace')
@@ -15,20 +14,13 @@ df_columna2=df.iloc[round(len(df)/2):,:]
 
 #imagen1=df['Imagenes_url'][0]+'.jpg'
 
-if st.button('Página'):
-    js = "window.open('https://www.streamlit.io/')"  # New tab or window
-    #js = "window.location.href = 'https://www.streamlit.io/'"  # Current tab
-    html = '<img src onerror="{}">'.format(js)
-    div = Div(text=html)
-    st.bokeh_chart(div)
-
 
 col1, col2= st.columns(2)
 
 with col1:
    for i in range(len(df_columna1)):
     
-    #st.button(df_columna1.iloc[i][0], help=df_columna1.iloc[i][2])
+    st.button(df_columna1.iloc[i][0], help=df_columna1.iloc[i][2])
     st.caption(df_columna1.iloc[i][1])
     #st.image(df_columna1.iloc[0][3], caption=df_columna1.iloc[0][3], use_column_width='auto')
     
