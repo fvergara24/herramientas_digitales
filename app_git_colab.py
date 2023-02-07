f39113486610e7da2673c6336b7e4e3060ea4a25
fvergara24 <fvergara@gmail.com>
2023-02-07 23:17:11 +0000

    agregar app

--- app_git_colab.py
+++ app_git_colab.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import pandas as pd
 import webbrowser
+from bokeh.models.widgets import Div
 
 st.set_page_config(page_title='Herramientas digitales en español')
 st.title('Empoweringrace')
@@ -19,6 +20,12 @@ col1, col2= st.columns(2)
 
 with col1:
    for i in range(len(df_columna1)):
+    if st.button('Página'):
+    js = "window.open('https://www.streamlit.io/')"  # New tab or window
+    js = "window.location.href = 'https://www.streamlit.io/'"  # Current tab
+    html = '<img src onerror="{}">'.format(js)
+    div = Div(text=html)
+    st.bokeh_chart(div)
     st.button(df_columna1.iloc[i][0], help=df_columna1.iloc[i][2])
     st.caption(df_columna1.iloc[i][1])
     #st.image(df_columna1.iloc[0][3], caption=df_columna1.iloc[0][3], use_column_width='auto')
