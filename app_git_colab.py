a160f1ab2f67fce7d58accafaeeabbb7c1af6103
fvergara24 <fvergara@gmail.com>
2023-02-07 05:42:01 +0000

    agregar app

--- app_git_colab.py
+++ app_git_colab.py
@@ -20,7 +20,7 @@ col1, col2= st.columns(2)
 with col1:
    for i in range(len(df_columna1)):
     if st.button(df_columna1.iloc[i][0]):
-      webbrowser.open_new_tab(df_columna1.iloc[i][2])
+        webbrowser.open_new_tab(df_columna1.iloc[i][2])
     #st.button(df_columna1.iloc[i][0], help=df_columna1.iloc[i][2])
     st.caption(df_columna1.iloc[i][1])
     #st.image(df_columna1.iloc[0][3], caption=df_columna1.iloc[0][3], use_column_width='auto')
