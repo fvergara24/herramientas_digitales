7772710c3cee5251a9cfbf64685e8509bbf33a49
fvergara24 <fvergara@gmail.com>
2023-02-18 00:48:42 +0000

    agregar app

--- app_git_colab.py
+++ app_git_colab.py
@@ -15,6 +15,25 @@ df_columna2=df.iloc[round(len(df)/2):,:]
 
 #imagen1=df['Imagenes_url'][0]+'.jpg'
 
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+def remote_css(url):
+    st.markdown(f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True)    
+
+def icon(icon_name):
+    st.markdown(f'<i class="material-icons">{icon_name}</i>', unsafe_allow_html=True)
+
+local_css("style.css")
+remote_css('https://fonts.googleapis.com/icon?family=Material+Icons')
+
+icon("search")
+selected = st.text_input("", "Search...")
+button_clicked = st.button("OK")
+
+
+
 st.text_input('Buscar aplicaciones')
 
 col1, col2= st.columns(2)
