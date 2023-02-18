cb233348b089d1345aa654e8c543403433c40e02
fvergara24 <fvergara@gmail.com>
2023-02-18 00:42:39 +0000

    agregar app

--- app_git_colab.py
+++ app_git_colab.py
@@ -15,7 +15,8 @@ df_columna2=df.iloc[round(len(df)/2):,:]
 
 #imagen1=df['Imagenes_url'][0]+'.jpg'
 
-st.text_input('Buscar aplicaciones')
+st.text_input('Buscar aplicaciones',value=df_columna1)
+
 col1, col2= st.columns(2)
 
 with col1:
