1515f8d43939176188afd1d02c6c2c548a6f811a
fvergara24 <fvergara@gmail.com>
2023-02-21 03:25:48 +0000

    agregar app

--- app_git_colab.py
+++ app_git_colab.py
@@ -20,19 +20,19 @@ st.markdown(f'buscaste {text}')
 if text:
   st.write('pasa')
 
-col1, col2= st.columns(2)
+#col1, col2= st.columns(2)
 
-with col1:
-   for i in range(len(df_columna1)):
-    st.write(f"{[df_columna1.iloc[i][0]]}(%s)" % df_columna1.iloc[i][2])
-    st.caption(df_columna1.iloc[i][1])
-    #st.image(df_columna1.iloc[0][3], caption=df_columna1.iloc[0][3], use_column_width='auto')
+#with col1:
+#   for i in range(len(df_columna1)):
+#    st.write(f"{[df_columna1.iloc[i][0]]}(%s)" % df_columna1.iloc[i][2])
+#    st.caption(df_columna1.iloc[i][1])
+#    #st.image(df_columna1.iloc[0][3], caption=df_columna1.iloc[0][3], use_column_width='auto')
     
-with col2:
-   for i in range(len(df_columna2)):
-    st.write(f"{[df_columna2.iloc[i][0]]}(%s)" % df_columna2.iloc[i][2])
-    st.caption(df_columna2.iloc[i][1])
-    #st.image(df_columna2.iloc[1][3], caption=df_columna2.iloc[1][2])
+#with col2:
+#   for i in range(len(df_columna2)):
+#    st.write(f"{[df_columna2.iloc[i][0]]}(%s)" % df_columna2.iloc[i][2])
+#    st.caption(df_columna2.iloc[i][1])
+#    #st.image(df_columna2.iloc[1][3], caption=df_columna2.iloc[1][2])
 
 
 mask = df['Descripcion'].str.contains(text)
