d917244663530336d9202c04b8e0e4bab4504878
fvergara24 <fvergara@gmail.com>
2023-02-21 03:37:24 +0000

    agregar app

--- app_git_colab.py
+++ app_git_colab.py
@@ -32,7 +32,7 @@ else:
   with col1:
      for i in range(len(df_columna1)):
       #original_title = '<p style="font-family:Courier; color:Blue; font-size: 20px;">f"{[df_columna1.iloc[i][0]]}(%s)" % df_columna1.iloc[i][2]</p>'
-      st.markdown(original_title, unsafe_allow_html=True) 
+      #st.markdown(original_title, unsafe_allow_html=True) 
       st.write(f"{[df_columna1.iloc[i][0]]}(%s)" % df_columna1.iloc[i][2])
       st.caption(df_columna1.iloc[i][1])
       #st.image(df_columna1.iloc[0][3], caption=df_columna1.iloc[0][3], use_column_width='auto')
