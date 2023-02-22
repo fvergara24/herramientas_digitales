3f2fe53d5beccf9ed5aa37348e6e38c7368e2286
fvergara24 <fvergara@gmail.com>
2023-02-22 00:06:25 +0000

    agregar app

--- app_git_colab.py
+++ app_git_colab.py
@@ -54,3 +54,20 @@ else:
         st.caption(df_columna2.iloc[i][1])
         #st.image(df_columna2.iloc[1][3], caption=df_columna2.iloc[1][2])
 
+
+# Using object notation
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
+
+# Using "with" notation
+with st.sidebar:
+    add_radio = st.radio(
+        "Choose a shipping method",
+        ("Standard (5-15 days)", "Express (2-5 days)")
+    )
+
+
+
+
